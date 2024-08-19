feea13f657074d8200fb1863a43fca3171b46b02
LuisNegretti <luisnegretti7@gmail.com>
2024-08-19 14:04:12 -0400

    primer commit

--- src/sistema/planta/SistemaPlanta.java
+++ src/sistema/planta/SistemaPlanta.java
@@ -12,16 +12,6 @@ package sistema.planta;
 public class SistemaPlanta {
 
     /**
-     * @param args the command line arguments
-     */
-     public static void main(String arg[]){
-   
-   int num_uno = 3;
-   int num_dos = 2;
-   int resultado = 0;
-
-   resultado = num_uno + num_dos;
-
-   System.out.println("El valor de tu suma es: " + resultado);   
+     *
  }
 }
